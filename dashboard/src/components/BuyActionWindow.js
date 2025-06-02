f0495c8fbe05506655c3275f080539912d97de82
Rishav kumar jha <110894378+rishavjha763@users.noreply.github.com>
2025-06-02 22:33:04 +0530

    Update BuyActionWindow.js

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodha-clone-1-1iv3.onrender.com", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
